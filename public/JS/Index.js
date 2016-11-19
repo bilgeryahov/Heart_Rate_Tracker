5e5c3afcc24d7761f222b5f82f5e5bccec0d868d
Bilger Yahov <bilger@studyportals.com>
2016-11-19 15:28:40 +0100

    Add mootools and Index.js file. Include them to the HTML file.

---
+++ public/JS/Index.js
@@ -0,0 +1,19 @@
+/**
+ * @file Index.js
+ *
+ * @author Bilger Yahov <bayahov1@gmail.com>
+ * @version 1.0.0
+ */
+
+var Index = {
+
+	init: function(){
+
+		
+	}
+};
+
+document.addEvent('domready', function(){
+
+	Index.init();
+});
